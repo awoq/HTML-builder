84adb208c16e0f705fe9fd0e4f6f834ab6b4a978
Alyona Chuikova <alyona.chuykova@gmail.com>
2024-01-18 02:14:02 +0000

    feat: add 02-write-file task solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const fullPath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(fullPath,  { flags: 'a' }, 'utf-8');
+const stdout = process.stdout;
+const stdin = process.stdin;
+
+stdout.write('Hello! Enter your text:\n');
+
+process.on('exit', () => stdout.write('Goodbye!\n'));
+process.on('SIGINT', () => process.exit());
+
+stdin.on('data', (data) => {
+  if (data.toString().toLowerCase().trim() === 'exit') {
+    process.exit();
+  } else writeStream.write(data);
+});
